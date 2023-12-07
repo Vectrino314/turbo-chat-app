fe81c3b6e2f7463d5116fb3815035426eab085d2
Vectrino314 <vectrino314@gmail.com>
2023-12-06 23:36:01 -0600

    Added Rooms Scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_045841) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_07_053054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
