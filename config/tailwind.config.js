9b3b47433e4d933cfb20f6014c1d88d289673c4e
Vectrino314 <vectrino314@gmail.com>
2024-01-01 19:52:41 -0600

    Added Sidebar and New Room Form
    Using Turbo Frames

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -12,6 +13,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'orange-500': colors.orange[500],
+      }
     },
   },
   plugins: [
