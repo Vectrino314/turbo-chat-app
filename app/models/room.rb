527b463d964da708340a7a9bd024625630d1e67d
Vectrino314 <vectrino314@gmail.com>
2024-01-02 12:48:50 -0600

    Added Relationship between User and Rooms
    New db migration for create_user_rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,6 @@
 class Room < ApplicationRecord
   validates :name, presence: true
+
+  has_many :user_rooms
+  has_many :users, through: :user_rooms
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_053054) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_02_181545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_053054) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_rooms", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_user_rooms_on_room_id"
+    t.index ["user_id"], name: "index_user_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_053054) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_rooms", "rooms"
+  add_foreign_key "user_rooms", "users"
 end
