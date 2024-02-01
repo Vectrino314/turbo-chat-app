d52236f2a2477ad2058e7b7ef49d329c1ec088e5
Victor Solis <vectrino314@gmail.com>
2024-01-31 22:29:16 -0600

    Fixed Chat Bubble Orientation

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_05_033834) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
